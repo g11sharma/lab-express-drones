c4c05b61f5d23706dd55ea41473b587956d511df
gaurav sharma <g11.sharma@gmail.com>
2022-06-04 23:07:22 +0200

    done

--- routes/drones.js
+++ routes/drones.js
@@ -2,25 +2,68 @@ const express = require('express')
 const router = express.Router()
 
 // require the Drone model here
+const Drone =  require('../models/Drone.model.js')
 
-router.get('/drones', (req, res, next) => {
+
+router.get('/drones', async (req, res, next) => {
+  try {
+    const allDrones = await Drone.find()
+    res.status(200).json(allDrones)
+  } catch (err) {
+    next(err)
+  }
+  
   // Iteration #2: List the drones
   // ... your code here
 })
 
-router.post('/drones', (req, res, next) => {
+router.post('/drones', async (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  const drone= req.body
+  const addOneDrone = await Drone.create(drone);
+  if (!drone.name){
+    res.status(400).json({
+      message: 'BAD REQUEST',
+    })
+    return
+  }
+  res.status(201).json({
+    message: 'CREATED ',
+    body: req.body,
+  })
+
 })
 
-router.post('/drones/:id', (req, res, next) => {
+router.post('/drones/:id', async(req, res, next) => {
+  try {
+    const droneId = req.params.id
+    // the "{new: true}" option allows us to get the updated version of the document instead of the previous one 
+    const updateddroneId = await Drone.findByIdAndUpdate(droneId, req.body, {new: true})
+    res.status(200).json(updateddroneId)
+} catch (err) {
+
+    next(err)
+}
+  
+  
   // Iteration #4: Update the drone
   // ... your code here
 })
 
-router.delete('/drones/:id', (req, res, next) => {
+router.delete('/drones/:id', async(req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
+  try {
+    const deletedThing = await Drone.findByIdAndDelete(req.params.id)
+    console.log(deletedThing)
+    res.json({message: `I deleted ${deletedThing.name}`})
+} catch (err) {
+
+    next(err)
+}
+
+
 })
 
 module.exports = router
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,20 @@
 // Iteration #1
+const drone = require('../models/Drone.model.js')
+
+const openConnection = require('../db/index.js')
+const { default: mongoose } = require('mongoose')
+
+
+const drones = [
+    { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+    { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+    { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 },
+  ]
+
+  async function seeddrone() {
+  const createDrones  = await drone.create(drones)
+  console.log(`Created ${createDrones.length} drones.`)
+  await mongoose.connection.close()
+  console.log('Connection closed.')
+  }
+  seeddrone()
